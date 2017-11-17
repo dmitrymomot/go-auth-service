ec361f7eb8ee4f70691007c1b156581a6af64270
Dmitry Momot <mail@dmomot.com>
2017-11-18 00:59:34 +0200

    Base methods

--- src/main.go
+++ src/main.go
@@ -18,6 +18,9 @@ import (
 
 // APP_ENV application environment
 // APP_HOST application host name
+// APP_PORT application internal port
+// APP_CACHE_DIR application cache directory, mainly using for let's encrypt certificates
+// DB_CONN database connection string, must contain full connection data like username, password. db name, etc
 const (
 	AppEnv          string = "APP_ENV"
 	AppHost         string = "APP_HOST"
@@ -53,11 +56,25 @@ func main() {
 	}
 	db.AutoMigrate(&User{}, &Role{})
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusPermanentRedirect, "/login")
 	})
+
+	r.GET("/login", LoginPageHandler)
+	r.GET("/register", RegisterPageHandler)
+	r.GET("/forgot-password", ForgotPasswordPageHandler)
+
+	r.POST("/login", LoginHandler)
+	r.POST("/register", RegisterHandler)
+	r.POST("/forgot-password", ForgotPasswordHandler)
+
+	API := r.Group("/API")
+	{
+		API.POST("/login", APILoginHandler)
+		API.POST("/register", APIRegisterHandler)
+		API.POST("/forgot-password", APIForgotPasswordHandler)
+		API.POST("/refresh-token", APIRefreshTokenHandler)
+	}
 
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusNotFound, gin.H{"error": http.StatusText(http.StatusNotFound)})
@@ -82,4 +99,76 @@ func main() {
 // recoveryHandler creates json response with error
 func recoveryHandler(c *gin.Context, err interface{}) {
 	c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+}
+
+// CheckErr is errors handler
+func checkErr(err error, c *gin.Context) {
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+}
+
+// LoginPageHandler login page
+func LoginPageHandler(c *gin.Context) {
+
+}
+
+// RegisterPageHandler registration page
+func RegisterPageHandler(c *gin.Context) {
+
+}
+
+// ForgotPasswordPageHandler reset password page, first step
+func ForgotPasswordPageHandler(c *gin.Context) {
+
+}
+
+// ResetPasswordPageHandler reset password page, second step
+func ResetPasswordPageHandler(c *gin.Context) {
+
+}
+
+// LoginHandler login user via web interface
+func LoginHandler(c *gin.Context) {
+
+}
+
+// RegisterHandler register new user via web interface
+func RegisterHandler(c *gin.Context) {
+
+}
+
+// ForgotPasswordHandler reset fogotten password via web interface, step one
+func ForgotPasswordHandler(c *gin.Context) {
+
+}
+
+// ResetPasswordHandler reset fogotten password via web interface, step one
+func ResetPasswordHandler(c *gin.Context) {
+
+}
+
+// APILoginHandler login user via API
+func APILoginHandler(c *gin.Context) {
+
+}
+
+// APIRegisterHandler register new user via API
+func APIRegisterHandler(c *gin.Context) {
+
+}
+
+// APIForgotPasswordHandler reset fogotten password via API, first step
+func APIForgotPasswordHandler(c *gin.Context) {
+
+}
+
+// APIResetPasswordHandler reset fogotten password via API, step two
+func APIResetPasswordHandler(c *gin.Context) {
+
+}
+
+// APIRefreshTokenHandler refresh API token
+func APIRefreshTokenHandler(c *gin.Context) {
+
 }
